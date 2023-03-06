8859e2e5aa9157cff29120b82caee5f427d8b5d3
Tigro <easterlingme95@gmail.com>
2023-03-06 14:18:28 -0600

    added basic structure of resolver file

---
+++ server/schema/resolvers.js
@@ -0,0 +1,13 @@
+const { Books, User } = require('../models');
+const { signToken } = require('../utils/auth');
+
+const resolvers = {
+    Query: {
+    },
+
+    Mutation: {
+
+    }
+};
+
+module.exports = resolvers;
